0eec4e7b8f57941f21e13e85eb688d6bef9396a6
J-F Bilodeau <jfbilodeau@chronogears.com>
2025-08-06 11:11:40 -0500

    Add user input functionality to greet by name

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿Console.WriteLine("Starting GitWitIt...");
 
+Console.Write("Please enter your name: ");
+var name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}!");
+
 Console.WriteLine("Done!");
